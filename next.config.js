33aa94d6995bf2c0803221707f3e994a13d84e4e
Kuldeep Dhaka <kdhaka94@gmail.com>
2023-09-18 15:33:09 +0530

    origin update

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: ["https://wanderleaf.in", "http://localhost:3000"],
+            value: "*",
           }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
